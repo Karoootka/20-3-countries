ca50de3e9c92d90cca81c21643e6730ab4a4cc15
Karolina Madej <kkarolinamadej@gmail.com>
2018-07-24 20:17:12 +0200

    Add country list component

--- src/presentational/flag.component.js
+++ src/presentational/flag.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CountryFlag = props => (
+const CountryFlag = (props) => (
   <div className="country-logo-wrapper">
     <img className="country-logo" src={props.country.imageUrl} alt="country photo" />
   </div>
